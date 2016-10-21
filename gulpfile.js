eeefa65c888135535df9798766ac6f282af55a32
vikas <vikasyadav.mech2009@gmail.com>
2016-10-21 22:44:17 +0530

    added coffee files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
-var gulp=require('gulp');
-	gutil=require('gulp-util');
+var gulp=require('gulp'),
+	gutil=require('gulp-util'),
+	coffee=require('gulp-coffee');
 
-gulp.task('log',function(){
 
-	gutil.log('Workflows are awesome!!');
+var coffeeSources=['component/coffee/tagline.coffee'];
+gulp.task('coffee',function(){
+	gulp.src(coffeeSources)
+	  .pipe(coffee({ bare : true })
+	  	.on('error',gutil.log))
+	  .pipe(gulp.dest('component/scripts'))
 });
 
